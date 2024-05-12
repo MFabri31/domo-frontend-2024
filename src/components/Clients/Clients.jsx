685096a2851c0f481970ca2ec284ad339fe27864
MFabri31 <medinajose3195@gmail.com>
2024-05-12 02:20:46 -0300

    Insert it news africa icon

--- src/components/Clients/Clients.jsx
+++ src/components/Clients/Clients.jsx
@@ -3,6 +3,7 @@ import NikeIcon from "../../assets/icons/nike-icon.svg";
 import GoogleIcon from "../../assets/icons/google-icon.svg";
 import AirbnbIcon from "../../assets/icons/airbnb-icon.svg";
 import TnwIcon from "../../assets/icons/tnw-icon.svg";
+import ItNewsAfricaIcon from "../../assets/icons/it-news-africa-icon.png";
 import "./Clients.css";
 
 const Clients = () => {
@@ -12,7 +13,7 @@ const Clients = () => {
         <Row className="text-center ">
           <Col md={12} className="">
             <h3 className="text-body-tertiary fw-bold">clients we worked it</h3>
-            <div className="d-flex flex-column flex-md-row justify-content-between">
+            <div className="d-flex flex-column flex-md-row justify-content-between align-items-center">
               <img src={TnwIcon} alt="Icon of tnw" className="img-fluid" />
               <img
                 src={GoogleIcon}
@@ -24,6 +25,12 @@ const Clients = () => {
                 src={AirbnbIcon}
                 alt="Icon of airbnb"
                 className="img-fluid"
+              />
+              <img
+                src={ItNewsAfricaIcon}
+                alt="Icon of it news africa"
+                className="img-fluid mt-5 mt-md-0"
+                style={{ width: "18rem", height: "3rem" }}
               />
             </div>
           </Col>
